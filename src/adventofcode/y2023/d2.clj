12c8b8ef638ccf569757af75758fcda1aec8e552
Charles Lechasseur <shiftingbeard@gmx.com>
2023-12-02 01:48:15 -0500

    Day 2

---
+++ src/adventofcode/y2023/d2.clj
@@ -0,0 +1,41 @@
+(ns adventofcode.y2023.d2
+  (:require [adventofcode.y2023.input.d2 :as d2-input]))
+
+(defn- parse-game
+  [game]
+  (let [[_ id reveals] (re-find #"^Game (\d+): (.*)$" game)]
+    (->> reveals
+         (re-seq #"[^; ][^;]*")
+         (map #(re-seq #"(\d+) (blue|red|green)" %))
+         (map #(map (fn [[_ count color]] (vector color (Integer/parseInt count))) %))
+         (map #(into (sorted-map) %))
+         (list (Integer/parseInt id)))))
+
+(def ^:private games
+  (map parse-game d2-input/games-input))
+
+(def ^:private part-1-targets
+  {"red" 12 "green" 13 "blue" 14})
+
+(defn part-1
+  []
+  (->> games
+       (filter
+         (fn [[_ reveals]]
+           (->> reveals
+                (map #(map (fn [[color count]] (<= count (part-1-targets color))) %))
+                flatten
+                (reduce #(and %1 %2)))))
+       (map first)
+       (apply +)))
+
+(defn part-2
+  []
+  (->> games
+       (map
+         (fn [[_ reveals]]
+           (->> reveals
+                (apply (partial merge-with max))
+                vals
+                (apply *))))
+       (apply +)))
--- test/adventofcode/y2023_test.clj
+++ test/adventofcode/y2023_test.clj
@@ -1,6 +1,7 @@
 (ns adventofcode.y2023-test
   (:require [clojure.test :refer :all]
-            adventofcode.y2023.d1))
+            adventofcode.y2023.d1
+            adventofcode.y2023.d2))
 
 (deftest day-1-part-1
   (testing "Day 1 part 1"
@@ -10,3 +11,13 @@
   (testing "Day 1 part 2"
     (let [value (adventofcode.y2023.d1/part-2)]
       (is (= value 54980)))))
+
+(deftest day-2-part-1
+  (testing "Day 2 part 1"
+    (let [value (adventofcode.y2023.d2/part-1)]
+      (is (= value 2377)))))
+(deftest day-2-part-2
+  (testing "Day 2 part 2"
+    (let [value (adventofcode.y2023.d2/part-2)]
+      (is (= value 71220)))))
+
