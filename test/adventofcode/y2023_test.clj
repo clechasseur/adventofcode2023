a60b155529deb02fa3711e6fe5804dd428d4b4e2
Charles Lechasseur <shiftingbeard@gmx.com>
2023-12-01 19:51:10 -0500

    Day 1

--- test/adventofcode/y2023_test.clj
+++ test/adventofcode/y2023_test.clj
@@ -5,4 +5,8 @@
 (deftest day-1-part-1
   (testing "Day 1 part 1"
     (let [value (adventofcode.y2023.d1/part-1)]
-      (is (= value 42)))))
+      (is (= value 55816)))))
+(deftest day-1-part-2
+  (testing "Day 1 part 2"
+    (let [value (adventofcode.y2023.d1/part-2)]
+      (is (= value 54980)))))
