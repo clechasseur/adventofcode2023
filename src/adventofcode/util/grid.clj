9591d04cb0613be0ee76c3788466f0ad58719372
Charles Lechasseur <shiftingbeard@gmx.com>
2023-12-03 23:47:47 -0500

    Small refactoring

--- src/adventofcode/util/grid.clj
+++ src/adventofcode/util/grid.clj
@@ -6,7 +6,7 @@
   [grid pt]
   (loop [dim (dec (count pt))
          grid grid]
-    (when-let [sub-grid (clojure.core/get grid (clojure.core/get pt dim))]
+    (when-let [grid (clojure.core/get grid (clojure.core/get pt dim))]
       (if (zero? dim)
-        sub-grid
-        (recur (dec dim) sub-grid)))))
+        grid
+        (recur (dec dim) grid)))))
