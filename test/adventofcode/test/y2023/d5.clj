0f0274b1a81f655ea33be852df0f7acd1d9a21c0
Charles Lechasseur <shiftingbeard@gmx.com>
2023-12-05 22:18:16 -0500

    Day 5

---
+++ test/adventofcode/test/y2023/d5.clj
@@ -0,0 +1,12 @@
+(ns adventofcode.test.y2023.d5
+  (:require [clojure.test :refer :all]
+            [adventofcode.y2023.d5 :as d5-s]))
+
+(deftest part1
+  (testing "Day 5 part 1"
+    (let [value (d5-s/part-1)]
+      (is (= value 650599855)))))
+(deftest part2
+  (testing "Day 5 part 2"
+    (let [value (d5-s/part-2)]
+      (is (= value 1240035)))))
